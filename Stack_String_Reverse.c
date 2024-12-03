3a0abfb24ef2d713756bf39d61dd78a9d55f6cef
SOURAV BOSE <souravbose361@gmail.com>
2024-12-03 12:54:01 +0530

    string reverse using stack code update

--- Stack_String_Reverse.c
+++ Stack_String_Reverse.c
@@ -2,33 +2,40 @@
 #include<stdlib.h>
 #include<string.h>
 #define size 100
-char b[size][50];
-int n,top = -1;
+char b[size][size];
+int n,top=-1;
 void push(char a[])
 {
+    int i,j=0,l=0;
     if(top == n-1)
     {
-        printf("Stack is Full!!!\n");
+        printf("stack is full!!!\n");
         return;
     }
-    strcpy(b[++top],a);
-    strrev(b[top]);
+    top++;
+    l = strlen(a)-1;
+    for(i=l;i>=0;i--)
+    {
+        b[top][j] = a[i];
+        j++;
+    }
+    b[top][j] = '\0';
 }
 void pop()
 {
     if(top == -1)
     {
-        printf("Stack is Empty!!!\n");
+        printf("stack is empty!!!\n");
         return;
     }
-    printf("Popped string = %s\n",b[top--]);
+    printf("popped element = %s\n",b[top--]);
 }
 void display()
 {
     int i;
     if(top == -1)
     {
-        printf("Stack is Empty!!!\n");
+        printf("stack is empty!!!\n");
         return;
     }
     for(i=top;i>=0;i--)
@@ -43,14 +50,14 @@ int main()
     scanf("%d",&n);
     while(1)
     {
-        printf("\n0.exit\n1.insert string\n2.Delete string\n3.Reverse string display\nEnter your choice : ");
+        printf("\n0.exit\n1.push\n2.pop\n3.display\nEnter your choice : ");
         scanf("%d",&s);
         if(s == 0)
             break;
         switch(s)
         {
             case 1:
-                printf("Enter the string : ");
+                printf("Enter string : ");
                 scanf("%s",a);
                 push(a);
                 break;
@@ -61,7 +68,8 @@ int main()
                 display();
                 break;
             default:
-                exit(0);
+                printf("invalid choice!!!\n");
         }
     }
+    return 0;
 }
